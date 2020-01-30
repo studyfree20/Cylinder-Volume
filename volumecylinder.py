bfbeea55cd109db4370f01b0ae7b351bf5ec22bd
studyfree20 <studyfree20@gmail.com>
2020-01-29 23:41:24 -0800

    cylindervolume

---
+++ volumecylinder.py
@@ -0,0 +1,14 @@
+'''
+requirements:
+pie
+height
+radius
+'''
+#code end here BSCIT-01-0345/2018
+height=int(input("Enter height of Cylinder   "))
+radius=int(input("Enter radius   "))
+pie=22/7
+volume=pie*radius*radius*height
+print("Volume is:  ")
+print (volume)
+#code end here BSCIT-01-0345/2018
